22af8a176f5182a6910a1ef42c736006c4b5110f
Simon Shvets <goodweather.spb@gmail.com>
2022-03-27 16:45:30 +0300

    Methods Split and Reverse are now implemented in Methods.cdproj

--- Skillbox/Methods/Program.cs
+++ Skillbox/Methods/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Methods
 {
     class Program
     {
+        private const string _separator = " ";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please insert any prase separated by empty space: \" \"");
+            var line = Console.ReadLine();
+            var lines = SplitLine(line);
+            WriteLines(lines);
+
+            Console.WriteLine();
+
+            var reverseLines = Reverse(line);
+            WriteLines(reverseLines);
+        }
+
+        private static IEnumerable<string> SplitLine(string line)
+        {
+            return line.Split(_separator);
+        }
+
+        private static void WriteLines(IEnumerable<string> lines)
+        {
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        private static IEnumerable<string> Reverse(string line)
+        {
+            return SplitLine(line).Reverse();
         }
     }
 }
